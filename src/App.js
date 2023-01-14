69e84e65d3f009564e8e9fe6ff9f29e8e2a2e148
Heath Henley <heath.j.henley@gmail.com>
2023-01-14 17:36:22 -0500

    Heroku create-react-app test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Countdown from 'react-countdown';
+
+// On Complete render this 
+function Completionist() {
+  return (
+    <span>You are good to go!</span>
+  );
+}
+
+// Renderer callback with condition
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a completed state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return <span>{days} days, {hours} hours, {minutes}, and {seconds} seconds</span>;
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countdown
+        date={'2023-07-14T17:00:00'}
+        renderer={renderer}/>
     </div>
   );
 }
