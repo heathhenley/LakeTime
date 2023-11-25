5ebf539fef188c6acfaebbe349f40c22ffae3267
Heath Henley <heath.j.henley@gmail.com>
2023-11-24 22:36:33 -0500

    New year new counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import './App.css';
 import Countdown from 'react-countdown';
 
+function StyledCountdown(
+  {title, days, hours, minutes, seconds}
+) {
+  return (
+    <section className="w-full w-min:400px">
+      <div className="container grid items-center justify-center gap-4 text-center lg:gap-10">
+        <div className="space-y-3">
+          <h2 className="text-3xl font-bold  sm:text-4xl md:text-5xl">{title}</h2>
+        </div>
+        <div className="grid grid-cols-4 divide-x divide-zinc-200">
+          <div className="flex flex-col items-center px-6">
+            <p className="md:text-6xl text-5xl font-bold">{days}</p>
+            <p className="text-zinc-500 md:text-3xl text-2xl">Days</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="md:text-6xl text-5xl font-bold">{hours}</p>
+            <p className="text-zinc-500 md:text-3xl text-2xl">Hours</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="md:text-6xl text-5xl font-bold">{minutes}</p>
+            <p className="text-zinc-500 md:text-3xl text-2xl">Minutes</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="md:text-6xl text-5xl font-bold">{seconds}</p>
+            <p className="text-zinc-500 md:text-3xl text-2xl">Seconds</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
 // On Complete render this 
 function Completionist() {
   return (
     <div className="finished">
-      <iframe src="https://giphy.com/embed/KHWAURFE2KcntpU5Et" width="270" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>It's Lake Time!</p>
+      <iframe src="https://giphy.com/embed/KHWAURFE2KcntpU5Et" width="270" height="480" frameBorder="0" title="lake time!" class="giphy-embed" allowFullScreen></iframe><p>It's Lake Time!</p>
     </div>
   );
 }
@@ -17,15 +49,20 @@ const renderer = ({ days, hours, minutes, seconds, completed }) => {
     return <Completionist />;
   } else {
     // Render a countdown
-    return <span>{days} days, {hours} hours, {minutes} minutes, and {seconds} seconds</span>;
+    return <StyledCountdown
+      title="Time until Lake Time"
+      days={days}
+      hours={hours}
+      minutes={minutes}
+      seconds={seconds} /> 
   }
 };
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col justify-center items-center">
       <Countdown
-        date={'2024-07-12T17:00:00'}
+        date={'2024-07-19T17:00:00'}
         renderer={renderer}/>
     </div>
   );
