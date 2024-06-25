717216f7c9dd20f36cede0ec50571522f76e2ab1
Heath Henley <heath.j.henley@gmail.com>
2024-06-25 18:25:35 -0400

    positioning and tooltip

--- src/App.js
+++ src/App.js
@@ -1,43 +1,65 @@
-import './App.css';
-import Countdown from 'react-countdown';
+import "./App.css";
+import Countdown from "react-countdown";
 
-function StyledCountdown(
-  {title, days, hours, minutes, seconds}
-) {
+function StyledCountdown({ title, days, hours, minutes, seconds, date }) {
   return (
-    <section className="w-full w-min:400px">
-      <div className="container grid items-center justify-center gap-4 text-center lg:gap-10">
+    <section className="w-full min-w-[400px]">
+      <div className="grid items-center justify-center gap-4 text-center lg:gap-10">
         <div className="space-y-3">
-          <h2 className="text-3xl font-bold  sm:text-4xl md:text-5xl">{title}</h2>
+          <div className="group flex relative justify-center">
+            <h2 className="text-3xl font-bold  sm:text-4xl md:text-5xl">
+              {title}
+            </h2>
+            <div>
+              <div
+                className="group-hover:opacity-100 transition-opacity bg-gray-600 p-2 text-sm text-gray-100 rounded-md absolute left-1/2 
+    -translate-x-1/2 -translate-y-full opacity-0 mx-auto"
+              >
+                <p>
+                  Laketime is {date} at{" "}
+                  <span className="font-bold text-red-500">MayhemManor</span>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="grid grid-cols-4 divide-x divide-zinc-200">
           <div className="flex flex-col items-center px-6">
             <p className="md:text-6xl text-5xl font-bold">{days}</p>
             <p className="text-zinc-500 md:text-3xl text-2xl">Days</p>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center px-6">
             <p className="md:text-6xl text-5xl font-bold">{hours}</p>
             <p className="text-zinc-500 md:text-3xl text-2xl">Hours</p>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center px-6">
             <p className="md:text-6xl text-5xl font-bold">{minutes}</p>
             <p className="text-zinc-500 md:text-3xl text-2xl">Minutes</p>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center px-6">
             <p className="md:text-6xl text-5xl font-bold">{seconds}</p>
             <p className="text-zinc-500 md:text-3xl text-2xl">Seconds</p>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-// On Complete render this 
+// On Complete render this
 function Completionist() {
   return (
     <div className="finished">
-      <iframe src="https://giphy.com/embed/KHWAURFE2KcntpU5Et" width="270" height="480" frameBorder="0" title="lake time!" class="giphy-embed" allowFullScreen></iframe><p>It's Lake Time!</p>
+      <iframe
+        src="https://giphy.com/embed/KHWAURFE2KcntpU5Et"
+        width="270"
+        height="480"
+        frameBorder="0"
+        title="lake time!"
+        class="giphy-embed"
+        allowFullScreen
+      ></iframe>
+      <p>It's Lake Time!</p>
     </div>
   );
 }
@@ -49,21 +71,23 @@ const renderer = ({ days, hours, minutes, seconds, completed }) => {
     return <Completionist />;
   } else {
     // Render a countdown
-    return <StyledCountdown
-      title="Time until Lake Time"
-      days={days}
-      hours={hours}
-      minutes={minutes}
-      seconds={seconds} /> 
+    return (
+      <StyledCountdown
+        title="Time until LakeTime&trade;"
+        days={days}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+        date={"5:00 PM on July 19th, 2024"}
+      />
+    );
   }
 };
 
 function App() {
   return (
     <div className="App flex flex-col justify-center items-center">
-      <Countdown
-        date={'2024-07-19T17:00:00'}
-        renderer={renderer}/>
+      <Countdown date={"2024-07-19T17:00:00"} renderer={renderer} />
     </div>
   );
 }
